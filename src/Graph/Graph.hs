a0c7cf854505d73b06962d7a7b3a66e900b28567
Dmitry Sluch <dmitry.sluch@yandex.ru>
2023-04-02 07:52:41 +0300

    wip

--- src/Graph/Graph.hs
+++ src/Graph/Graph.hs
@@ -1,30 +1,22 @@
 {-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE GADTs #-}
 
 module Graph.Graph where
 
 import Control.Monad.Free (Free)
 import Data.Matrix (Matrix)
 import Graph.GMatrix
+import Layers.Layer
 
-data Operation t a = 
+data Operation t a where
    -- | Input tensor.
-    Const (GMatrix t)
-   -- | Sum with a tensor.
-    | Sum a a
-   -- | Multiply by a tensor.
-    | Prod a a
-   -- | Linear layer with a tensor weight.
-    | Linear a (GMatrix t)
-   -- | ReLU activation.
-    | ReLU a
-   -- | Cross-entropy loss with a target tensor.
-    | CrossEntropyLogits a (GMatrix t) deriving (Functor)
+    Operation :: (Layer l t) => l -> a -> Operation t a
 
 -- | Computational graph with operations from grammar @f@.
 --   Intermediate results are cached for use in backward.
 type Graph f t = Free f (Matrix t)
 
-data OperationWithCache t a = OperationWithForward (Operation t a) (Matrix t) (Matrix t )deriving (Functor)
+
 
 
 
