f9e379c9a6fe86e3e1865a57052d3cda9dd07a74
Dmitry Sluch <dmitry.sluch@yandex.ru>
2023-04-02 04:56:06 +0300

    matrix

--- src/graph/graph.hs
+++ src/graph/graph.hs
@@ -1,23 +1,26 @@
 module Graph.Graph where
 
 import Control.Monad.Free (Free)
-import Data.Tensor (Tensor)
-import Graph.GTensor (GTensor)
+import Data.Matrix(Matrix)
+import Graph.GMatrix
 
-data Operation a s n
-  = -- | Input tensor.
-    Const (GTensor s n)
-  | -- | Sum with a tensor.
-    Sum a (GTensor s n)
-  | -- | Multiply by a tensor.
-    Prod a (GTensor s n)
-  | -- | Linear layer with a tensor weight.
-    Linear a (GTensor s n)
-  | -- | ReLU activation.
-    ReLU a
-  | -- | Cross-entropy loss with a target tensor.
-    CrossEntropyLogits a (GTensor s n)
+data Operation t a = 
+   -- | Input tensor.
+    Const (GMatrix t)
+   -- | Sum with a tensor.
+    | Sum a a
+   -- | Multiply by a tensor.
+    | Prod a a
+   -- | Linear layer with a tensor weight.
+    | Linear a (GMatrix t)
+   -- | ReLU activation.
+    | ReLU a
+   -- | Cross-entropy loss with a target tensor.
+    | CrossEntropyLogits a (GMatrix t)
 
 -- | Computational graph with operations from grammar @f@.
 --   Intermediate results are cached for use in backward.
-type Graph f s n = Free f (Maybe (Tensor s n))
+type Graph f t = Free f (Maybe (Matrix t))
+
+-- Computes the forward pass
+
