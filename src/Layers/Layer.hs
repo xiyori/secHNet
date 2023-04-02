b91776c12478f9a273665b1a3528a19e963a3a26
SergejVolkov <volkovsergej17@gmail.com>
2023-04-02 07:50:00 +0300

    Layer template

--- src/Layers/Layer.hs
+++ src/Layers/Layer.hs
@@ -1,10 +1,54 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE InstanceSigs #-}
+
 module Layers.Layer where
 
-import Data.Matrix
+import Data.Matrix (Matrix)
 
-class NNLayer t where
-    -- input tensor -> output tensor
-    forward :: Matrix t -> Matrix t
-    -- input tensor -> output returned by Layer on forward call -> gradient over outputs -> gradient over inputs
-    backward :: Matrix t -> Matrix t -> Matrix t -> Matrix t
+class Layer t a where
+    -- | layer -> input tensor -> (updated layer, output tensor)
+    forward :: a -> Matrix t -> (a, Matrix t)
+    -- | operation -> gradient over outputs ->
+    --   updated layer -> gradient over inputs
+    backward :: a -> Matrix t -> (a, Matrix t)
 
+data Linear t = Linear {
+  linearWeight :: Matrix t,
+  linearGradWeight :: Matrix t,
+  linearBias :: Matrix t,
+  linearGradBias :: Matrix t,
+  linearInput :: Matrix t
+}
+
+instance Layer t (Linear t) where
+  forward :: Linear t -> Matrix t -> (Linear t, Matrix t)
+  forward  = _
+
+  backward :: Linear t -> Matrix t -> (Linear t, Matrix t)
+  backward x = _
+
+newtype ReLU t = ReLU {
+  reluInput :: Matrix t
+}
+
+instance Layer t (ReLU t) where
+  forward :: ReLU t -> Matrix t -> (ReLU t, Matrix t)
+  forward  = _
+
+  backward :: ReLU t -> Matrix t -> (ReLU t, Matrix t)
+  backward x = _
+
+data CrossEntropyLogits t = CrossEntropyLogits {
+  сrossEntropyTarget :: Matrix t,
+  сrossEntropyInput :: Matrix t
+}
+
+instance Layer t (CrossEntropyLogits t) where
+  forward :: CrossEntropyLogits t -> Matrix t -> (CrossEntropyLogits t, Matrix t)
+  forward  = _
+
+  backward :: CrossEntropyLogits t -> Matrix t -> (CrossEntropyLogits t, Matrix t)
+  backward x = _
+
+setCrossEntropyTarget = _
