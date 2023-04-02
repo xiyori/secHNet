735665b85658d35005f12e59064266da18c375b2
Dmitry Sluch <dmitry.sluch@yandex.ru>
2023-04-02 08:50:13 +0300

    forward and backward

--- src/Gradient/Gradient.hs
+++ src/Gradient/Gradient.hs
@@ -1,13 +1,26 @@
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE ExistentialQuantification #-}
 module Gradient.Gradient where
-
+import Data.Matrix
 import Layers.Layer
 
-newtype LayerT t where
-    LayerT :: (Layer l t) => l -> LayerT t
+data LayerT t = forall l. (Layer t l) => LayerT l
 
-newtype Network t = { getNetwork :: [LayerT t] }
+newtype Network t = Network{ getNetwork :: [LayerT t] }
 
-forward :: Network t -> t -> (Network t, t)
-forward (Network layers) input = 
-    let res = foldr  [[], input]
+
+forwardOne :: ([LayerT t], Matrix t) -> (LayerT t) -> ([LayerT t], Matrix t)
+forwardOne (prefix, val) (LayerT l) = 
+    let (newLayer, newVal) = forward l val in 
+        (prefix ++ [LayerT newLayer], newVal)
+
+forwardNet :: Network t -> Matrix t -> (Network t, Matrix t)
+forwardNet (Network layers) input = 
+    let (layers, loss) = foldl forwardOne ([], input) layers in (Network layers, loss)
+
+backwardOne :: (LayerT t) -> ([LayerT t], Matrix t) -> ([LayerT t], Matrix t)
+backwardOne (LayerT l) (postfix, val) = 
+    let (newLayer, newVal) = backward l val in ((LayerT newLayer) : postfix, newVal) 
+
+backwardNet :: Network t -> Matrix t -> (Network t, Matrix t)
+backwardNet (Network layers) grad = 
+    let (layers, loss) = foldr backwardOne ([], grad) layers in (Network layers, loss)
