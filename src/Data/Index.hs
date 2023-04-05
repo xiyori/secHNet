b370db58f4cba36fe4a74a9016b44cacd1f3bfa1
xiyori <foma@shipilov.ru>
2023-04-05 11:31:58 +0300

    Tensor

---
+++ src/Data/Index.hs
@@ -0,0 +1,87 @@
+module Data.Index where
+
+import Data.List (foldl')
+
+type Index = [Int]
+type MIndex = (Int, Int)
+type MultiIndex = [[Int]]
+
+-- | Separate last 2 elements of a tensor index.
+splitIndex :: Index -> (Index, MIndex)
+splitIndex = go []
+  where
+    go [] [] = error "wrong index dimensions"
+    go [] [i] = ([1], (i, 1))
+    go [] [i, j] = ([1], (i, j))
+    go first [i, j] = (reverse first, (i, j))
+    go first (i : is) = go (i : first) is
+
+-- | Convert array and matrix indices to tensor index.
+mergeIndex :: Index -> MIndex -> Index
+mergeIndex arrayIndex (i, j) = arrayIndex ++ [i, j]
+
+-- | Convert tensor index to internal representation.
+toInternal :: Index -> Index -> (Int, MIndex)
+toInternal shape index =
+  (toInt (fst $ splitIndex shape) arrayIndex, matrixIndex)
+  where
+    (arrayIndex, matrixIndex) = splitIndex index
+
+-- | Convert tensor index to flat integer index.
+toInt :: Index -> Index -> Int
+toInt shape index =
+  foldl' (\accum (coeff, i) -> accum + coeff * (i - 1)) 1
+  $ zip (_dimCoeffs shape) index
+
+-- | Convert flat integer index to tensor index.
+fromInt :: Index -> Int -> Index
+fromInt shape index =
+  snd . foldl' (
+    \(i, accum) coeff -> (mod i coeff, div i coeff : accum)
+  ) (index, [])
+  $ _dimCoeffs shape
+
+_dimCoeffs :: [Int] -> [Int]
+_dimCoeffs =
+  foldr (\dim coeffs@(coeff : _) -> dim * coeff : coeffs) [1] . tail
+
+-- | Generate all indices between low and high (inclusive).
+indexRange :: Index -> Index -> [Index]
+indexRange low high = reverse $ go [low]
+  where
+    go range@(lastIndex : _) =
+      if nextIndex == high then
+        range
+      else nextIndex : range
+      where
+        nextIndex =
+          reverse
+          $ snd
+          $ foldr (
+            \(l, h, i) (needAdd, index) ->
+              if needAdd && i == h - 1 then
+                (True, l : index)
+              else if needAdd then
+                (False, i + 1 : index)
+              else (False, i : index)
+          ) (True, [])
+          $ zip3 low high lastIndex
+
+-- | Generate all indices between 1 and high (inclusive).
+indexRange0 :: Index -> [Index]
+indexRange0 high = indexRange (map (const 1) high) high
+
+-- | Total number of elements with indices between 1 and high.
+countIndex :: Index -> Int
+countIndex = foldl' (*) 1
+
+-- | Swap index dimensions.
+swapElementsAt :: Int -> Int -> Index -> Index
+swapElementsAt i j index =
+  left ++ [elemJ] ++ middle ++ [elemI] ++ right
+  where
+    elemI = index !! i
+    elemJ = index !! j
+    left = take i index
+    middle = take (j - i - 1) (drop (i + 1) index)
+    right = drop (j + 1) index
--- src/Data/Tensor.hs
+++ src/Data/Tensor.hs
@@ -1,5 +1,99 @@
 module Data.Tensor where
 
-import Data.Matrix
+import Control.Applicative
+import Data.Array (array, Array, (!))
+import qualified Data.Array as A
+import Data.Matrix (matrix, Matrix, (!))
+import qualified Data.Matrix as M
+import Data.Index
 
-data Tensor t = Tensor {shape :: [Int], tenData :: [Matrix t]}
+data Tensor t = Tensor {
+  shape :: Index,
+  tensorData :: Array Int (Matrix t)
+}
+
+tensor :: Index -> (Index -> t) -> Tensor t
+tensor shape builder =
+  Tensor shape
+  $ array (1, toInt arrayShape arrayShape)
+  $ map (
+    \index -> (
+      toInt arrayShape index,
+      matrix matrixRows matrixCols $ builder . mergeIndex index
+    ))
+  $ indexRange0 arrayShape
+  where
+    (arrayShape, (matrixRows, matrixCols)) = splitIndex shape
+
+full :: Index -> t -> Tensor t
+full shape value = tensor shape $ const value
+
+zeros :: Num t => Index -> Tensor t
+zeros shape = full shape 0
+
+ones :: Num t => Index -> Tensor t
+ones shape = full shape 1
+
+eye :: Num t => Index -> Tensor t
+eye shape =
+  tensor shape (
+    \index ->
+      if all (== head index) $ tail index then
+        1
+      else 0
+  )
+
+getElem :: Tensor t -> Index -> t
+getElem (Tensor shape dat) index = (dat A.! arrayIndex) M.! matrixIndex
+  where
+    (arrayIndex, matrixIndex) = toInternal shape index
+
+(!?) :: Tensor t -> Index -> t
+(!?) = getElem
+
+getElems :: Tensor t -> MultiIndex -> Tensor t
+getElems x@(Tensor shape _) multiIndex =
+  tensor newShape (\index -> x !? liftA2 (!!) expandedIndex index)
+  where
+    expandedIndex =
+      zipWith (
+        \dim slice ->
+          if null slice then
+            [1 .. dim]
+          else slice
+      ) shape multiIndex
+    newShape = map length expandedIndex
+
+numel :: Tensor t -> Int
+numel (Tensor shape _) = countIndex shape
+
+transpose :: Tensor t -> Tensor t
+transpose (Tensor shape dat) =
+  Tensor shape $ fmap M.transpose dat
+
+flatten :: Tensor t -> Tensor t
+flatten x@(Tensor shape dat) =
+  tensor [numel x] $ (x !?) . fromInt shape . head
+
+sum :: Num t => Tensor t -> t
+sum (Tensor _ dat) =
+  foldl (\accum mat -> accum + Prelude.sum mat) 0 dat
+
+swapdim :: Tensor t -> Int -> Int -> Tensor t
+swapdim x@(Tensor shape _) from to =
+  tensor (swapElementsAt from to shape) ((x !?) . swapElementsAt from to)
+
+dot :: Num t => Tensor t -> Tensor t -> Tensor t
+dot (Tensor shape1 dat1) (Tensor shape2 dat2) =
+  Tensor (mergeIndex arrayShape (matrixRows1, matrixCols2))
+  $ array (1, toInt arrayShape arrayShape)
+  $ map ((
+    \index -> (
+      index,
+      (dat1 A.! index) * (dat2 A.! index)
+    )) . toInt arrayShape)
+  $ indexRange0 arrayShape
+  where
+    (arrayShape, (matrixRows1, _)) = splitIndex shape1
+    (_, (_, matrixCols2)) = splitIndex shape2
+
